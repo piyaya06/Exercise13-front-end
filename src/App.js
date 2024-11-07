420e32739586b6850bf90ca7f8662450adfc9a05
piyaya06 <monoapril7@gmail.com>
2024-11-07 21:52:48 +0800

    Adding Exercise 13

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Header  from "./components/Header";
+import Clients from "./components/Clients";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="body-wrap">
+      <Header/>
+      <Clients/>
+      <Features/>
+      <Pricing/>
+      <Footer/>
+
+  <main className="site-content">
+    
+  </main>
+  </div>
+  );   
+};
 
 export default App;
